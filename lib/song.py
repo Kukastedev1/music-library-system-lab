891598e68bde096f85931b6460dd8e77fa2385f9
Kukastedev1 <kukasste@gmail.com>
2026-02-24 08:11:39 +0300

    Second commit

--- lib/song.py
+++ lib/song.py
@@ -1,25 +1,21 @@
 class Song:
-    # Class attributes (shared by all Song objects)
+    # Class attributes
     count = 0
     genres = set()
     artists = set()
     genre_count = {}
-    artists_count = {}
+    artist_count = {}  
 
     def __init__(self, name, artist, genre):
-        # Instance attributes (unique to each Song)
         self.name = name
         self.artist = artist
         self.genre = genre
 
-        # Update class-level statistics whenever a song is created
         Song.add_song_to_count()
         Song.add_to_genres(genre)
         Song.add_to_artists(artist)
         Song.add_to_genre_count(genre)
-        Song.add_to_artists_count(artist)
-
-    # Class Methods
+        Song.add_to_artist_count(artist)   
 
     @classmethod
     def add_song_to_count(cls):
@@ -41,41 +37,8 @@ class Song:
             cls.genre_count[genre] = 1
 
     @classmethod
-    def add_to_artists_count(cls, artist):
-        if artist in cls.artists_count:
-            cls.artists_count[artist] += 1
+    def add_to_artist_count(cls, artist):   
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
         else:
-            cls.artists_count[artist] = 1
-
-    # Instance method to display song info
-    def display_info(self):
-        return f"Song: {self.name}, Artist: {self.artist}, Genre: {self.genre}"
-
-    # Class method to display all statistics
-    @classmethod
-    def display_statistics(cls):
-        return {
-            "Total Songs": cls.count,
-            "Unique Genres": list(cls.genres),
-            "Unique Artists": list(cls.artists),
-            "Songs per Genre": cls.genre_count,
-            "Songs per Artist": cls.artists_count
-        }
-
-
-# Example usage:
-
-# Build song objects
-song1 = Song("Blinding Lights", "The Weeknd", "Pop")
-song2 = Song("Shape of You", "Ed Sheeran", "Pop")
-song3 = Song("HUMBLE", "Kendrick Lamar", "Hip-Hop")
-
-# See information about individual songs
-print(song1.display_info())
-print(song2.display_info())
-print(song3.display_info())
-
-print()
-
-# See information about all songs
-print(Song.display_statistics())
+            cls.artist_count[artist] = 1
